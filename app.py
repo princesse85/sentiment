66e4358340b99d1ad66efee41d58099860df4525
Princesse MBAMBI <princiadj@gmail.com>
2025-06-02 03:48:03 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import joblib
 import pandas as pd
 
-st.set_page_config(page_title="Sentiment Analysis", page_icon="💬", layout="wide")
+st.set_page_config(page_title="sentiment", page_icon="💬", layout="wide")
 
 #Load Models and Vectorizer
 @st.cache_resource
